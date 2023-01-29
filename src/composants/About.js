390859e4bac0770c70d2a31898b3f75910bc18da
niemazm <milo.niemaz@gmail.com>
2023-01-29 17:48:25 +0100

    Create About.js

---
+++ src/composants/About.js
@@ -0,0 +1,13 @@
+export default function About(){
+    return (
+        <div className="about">
+            <h1>About</h1>
+            <p>
+                Hi, I'm Milo Niemaz, a sophomore informatic student in IUT2 of Grenoble 38000.
+                I'm having a lot of fun learning about web development and I'm looking 
+                forward to learn more about it. That's why I'm doing this business card using React.
+            </p>
+            
+        </div>
+    );
+}
