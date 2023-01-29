1148802efe4ca06ae438f41b6a715285228210e3
niemazm <milo.niemaz@gmail.com>
2023-01-29 17:48:37 +0100

    Create Interests.js

---
+++ src/composants/Interests.js
@@ -0,0 +1,14 @@
+export default function Interests(){
+    return (
+        <div className="interests">
+            <h1>Interests</h1>
+            <p>
+                Musculation adict. 
+                Football fanatic. 
+                Movie lover. 
+                Anime fan. 
+                Knowledge seeker. 
+            </p>
+        </div>
+    );
+}
